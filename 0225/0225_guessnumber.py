d9604bbd4887e1b539d715800d30da4d19bbb26a
xiangyuwang1111 <157940291+xiangyuwang1111@users.noreply.github.com>
2024-02-28 05:14:00 +0000

    modify

--- 0225/0225_guessnumber.py
+++ 0225/0225_guessnumber.py
@@ -1,19 +1,32 @@
+#自定義function
 import random
 import pyinputplus as pyip
-print("========猜數字遊戲========\n")
-x=random.randint(1,100)
-count=0
-m=1
-M=100
-while(1):
-    ans=pyip.inputInt(f"猜數字({m}-{M}):",min=m,max=M)
-    count+=1
-    if(ans<x):
-        print("大一點!")
-        m=ans
-    elif(ans>x):
-        print("小一點!")
-        M=ans
-    else:
-        print(f"猜中了!猜了{count}次\n")
-        break
+def playgame():
+    print("========猜數字遊戲========\n")
+    x=random.randint(1,100)
+    count=0
+    m=1
+    M=100
+    while(1):
+        ans=pyip.inputInt(f"猜數字({m}-{M}):",min=m,max=M)
+        count+=1
+        if(ans<x):
+            print("大一點!")
+            m=ans
+        elif(ans>x):
+            print("小一點!")
+            M=ans
+        else:
+            print(f"猜中了!猜了{count}次\n")
+            break
+def main():
+    while(1):
+        playgame()
+        menu_value=pyip.inputMenu(["yes","no"],prompt="還要繼續嗎?\n",numbered=True)
+        if(menu_value=="no"):break
+    print("遊戲結束")
+#main()
+
+#python專案的主執行檔
+if(__name__=="__main__"):
+    main()
