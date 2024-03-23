76d1404c6be9e1a198f9e13f7e55badd7cc3ab56
xiangyuwang1111 <157940291+xiangyuwang1111@users.noreply.github.com>
2024-03-23 17:13:30 +0000

    modify

--- 0310/0310.py
+++ 0310/0310.py
@@ -1,11 +1,16 @@
 import random
 import pyinputplus as pyip
 from pprint import pprint 
-num=pyip.inputNum('How many students?')
-students:list[dict]=[]
-for _ in range(num):
-    student:dict[str:int]={}
-    for subject in ['Chinese','English','Math']:
-        student[subject]=random.randint(50,100)
-    students.append(student)
-pprint(students)
+
+def get_student(n):
+    students:list[dict]=[]
+    for _ in range(n):
+        student:dict[str:int]={}
+        for subject in ['Chinese','English','Math']:
+            student[subject]=random.randint(50,100)
+        students.append(student)
+    return students
+
+num=pyip.inputNum('How many students: ')
+stus=get_student(num)
+pprint(stus)
