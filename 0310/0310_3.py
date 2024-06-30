f42ced6d7d22f6c95489d7af3d31bf542c69a289
xiangyuwang1111 <157940291+xiangyuwang1111@users.noreply.github.com>
2024-06-30 15:02:01 +0000

    modify

--- 0310/0310_3.py
+++ 0310/0310_3.py
@@ -11,21 +11,21 @@ def get_student(n):
             student[subject]=random.randint(50,100)
         students.append(student)
     return students
-def save_file(filename:str,data:list[dict]):
+def save_file(fn:str,data:list[dict]):
     '''
     可以在這邊說明 
     數標移到function會出現說明
     '''
-    with open(csvname,mode='w',encoding='utf-8',newline='') as csvfile:
+    with open(fn,mode='w',encoding='utf-8',newline='') as csvfile:
         writer=csv.DictWriter(csvfile,fieldnames=['國文','英文','數學'])
         writer.writeheader()
-        writer.writerows(students)
-    print(f'{csvname}存檔完成')
+        writer.writerows(data)
+    print(f'{fn}存檔完成')
 def main():
     num=pyip.inputNum('輸入多少學生: ')
     students=get_student(num)
     filename=input('輸入檔案名稱:')
-    csvname=f'{filename}.csv'
+    csvnames=f'{filename}.csv'
     save_file(filename,students)
 
 main()
